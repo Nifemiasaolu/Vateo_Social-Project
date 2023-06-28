9069c939e459d3d9bbfdd429ccf21179147e9c54
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-06-28 18:31:39 +0100

    Click Functionality

--- main.js
+++ main.js
@@ -1,46 +1,53 @@
-"use strict"
+"use strict";
 
 // SIDEBAR
 
-    const menuItems = document.querySelectorAll('.menu_item');
-    const notificationPopUp = document.querySelector('.notification_popup');
-    const notificationCount = document.querySelector('.notification_count');
-    const messageNotificationCount = document.querySelector('.messages_notification_count');
+const menuItems = document.querySelectorAll(".menu_item");
+const notificationPopUp = document.querySelector(".notification_popup");
+const notificationCount = document.querySelector(".notification_count");
 
+// MESSAGES
+const messagesNotifications = document.querySelector("#messages_notifications");
+const messages = document.querySelector(".messages");
+const messageNotificationCount = document.querySelector(
+    ".messages_notification_count"
+);
 
-    // ======================= SIDEBAR ======================= 
+// Remove Box Shadow Onclick of Other Menu
+const noMessagesBoxShadow = () => (messages.style.boxShadow = "none");
 
-    // Remove active class from all Menu Item
-    const changeActiveItem = () => {
-        menuItems.forEach(item => {
-            item.classList.remove('active');
-        })
-    }
+// ======================= SIDEBAR =======================
 
+// Remove active class from all Menu Item
+const changeActiveItem = () => {
+    menuItems.forEach((item) => {
+        item.classList.remove("active");
+    });
+};
 
-    // Selecting one Menu Item at a time 
-    menuItems.forEach(item => {
-        item.addEventListener('click', () => {
-            changeActiveItem();
-            item.classList.add('active');
+// Selecting one Menu Item at a time
+menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        changeActiveItem(); //Call back function to remove 'active' from items
+        item.classList.add("active"); //Adds active for each item clicked
 
-            if(item.id != 'notifications') {
-                notificationPopUp.style.display = 'none';
-            } else {
-                notificationPopUp.style.display = 'block';
-                notificationCount.style.display = 'none';
-            }
-        })
-    })
+        if (item.id != "notifications") {
+            notificationPopUp.style.display = "none";
+            noMessagesBoxShadow();
+        } else {
+            notificationPopUp.style.display = "block";
+            notificationCount.style.display = "none";
+            noMessagesBoxShadow();
+        }
+    });
+});
 
-    // ======================= MESSAGES ======================= 
+// ======================= MESSAGES =======================
 
-    menuItems.forEach(item => {
-        item.addEventListener('click', () => {
-            if(item.id === 'messages_notifications') {
-                messageNotificationCount.style.display = 'none';
-                document.querySelector('.messages').style.box-shadow = '0,0,0,$black-color';
-            }
-        })
-
-    })
+messagesNotifications.addEventListener("click", () => {
+    messages.style.boxShadow = "0 0 0.6rem #6633CC";
+    messageNotificationCount.style.display = "none";
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+});
